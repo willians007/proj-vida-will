c931b3527558c9ef784021150e7a76f03d0da20a
Willian Brand Engels <107199164+willians007@users.noreply.github.com>
2024-04-03 12:20:28 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+const botoes = document.querySelectorALL(".botao");
+
+for (let i=0; i<botoes.length; i++)  {
+    botoes[i].onclick = function () {
+
+        for (let j=0; j<botoes.length; j++) {
+            botoes[j].classList.remove("ativo");
+        }
+
+        botoes[i].classList.add("ativo")
+    } 
+
+
+
+
+
+}
